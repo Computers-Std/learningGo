52a2452eb0e787425c60ef51bd6ce6dfb4d71b58
ukiran03 <ushakiranreddi@gmail.com>
2025-06-12 19:31:20 +0530

    Completed: Chapter 02 Predeclared Types and Declarations

---
+++ ch01/hello.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+// main ...
+func main() {
+	fmt.Printf("Hello, %s!\n", "world")
+}
---
+++ ch02/complex_numbers/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+func main() {
+	// creates a complex number 2.5 + 3.1i
+	x := complex(2.5, 3.1)
+
+	// creates a complex number 10.2 + 2.0i
+	y := complex(10.2, 2)
+
+	// basic arithmetic operations on complex numbers
+	fmt.Println(x + y)
+	fmt.Println(x - y)
+	fmt.Println(x * y)
+	fmt.Println(x / y)
+
+	// use the built-in real function to extract the real portion of a complex number
+	fmt.Println(real(x))
+
+	// use the built-in imag function to extract the imaginary portion of a complex number
+	fmt.Println(imag(x))
+
+	// use the Abs function in math/cmplx pkg to take the absolute value of a complex number
+	fmt.Println(cmplx.Abs(x))
+}
